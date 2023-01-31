3309226724cf681d727da64fcca907f202628288
mycomputer <Danil12.07.2012@gail.com>
2023-01-31 19:48:01 +0300

    Исправление ошибок, попытка 2)

--- src/Main.java
+++ src/Main.java
@@ -29,22 +29,19 @@ public class Main {
         System.out.println("Задача 2");
         int[] arr = generateRandomArray();
         int maxWastes = 99_000;
-
+        int minWastes = 201_000;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > maxWastes) {
                 maxWastes = arr[i];
             }
-        }
-
-        int minWastes = 201_000;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (minWastes > arr[i]) {
+            if (arr[i] < minWastes){
                 minWastes = arr[i];
             }
+
         }
         System.out.println("Минимальная сумма трат за день составила " + minWastes + " рублей. Максимальная сумма трат за день составила " + maxWastes + " рублей.");
     }
+
     public static void task3 () {
         System.out.println("Задача 3");
         int[] arr = generateRandomArray();
@@ -52,17 +49,13 @@ public class Main {
         for (int j : arr) {
             sum = sum + j;
         }
-        double average = sum / 30;
+        double average = (double)sum / arr.length;
         System.out.println("Средняя сумма трат за месяц составила " + average + " рублей.");
     }
     public static void task4 () {
         System.out.println("Задача 4");
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int index = reverseFullName.length -1; index >= 0 ; index--) {
-            if (index == 0) {
-                System.out.print(reverseFullName[index]);
-                break;
-            }
             System.out.print(reverseFullName[index]);
     }}}
 
